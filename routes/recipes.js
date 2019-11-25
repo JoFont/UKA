484634ee58ecd5fe99348355481c0b38dfa25243
Diogo Marques <diogo@fontainhas.com>
2019-11-25 12:38:41 +0000

    Query filtering

--- routes/recipes.js
+++ routes/recipes.js
@@ -5,25 +5,25 @@ const appId = process.env.EDAMAM_APP_ID;
 const appKey = process.env.EDAMAM_APP_KEY;
 
 router.get('/', async (req, res, next) => {
-    const includeCount = req.query.include.split(",");
-    const maxIngr = req.params.strict === "on" ? includeCount.length : "";
+    const includeCount = req.query.include.split(",").length;
+    const maxIngr = req.query.strict === "on" ? includeCount : "100";
 
-    console.log(maxIngr);
-
-    const include = req.query.include.split(",").join("+");
-    const exclude = req.query.exclude ? req.query.exclude.split(",").join("+") : "";
+    const include = req.query.include.split(",").map(el => el.trim()).join("+");
+    const exclude = req.query.exclude ? req.query.exclude.split(",").map(el => el.trim()).join("+") : "";
 
     try {
         const response = await axios.get(`https://api.edamam.com/search`, {
             params: {
                 q: include,
                 app_id: appId,
+                to: 30,
                 app_key: appKey,
                 exclude: exclude,
-                // ingr: maxIngr
+                ingr: maxIngr
             }
         });
-        res.render("recipes", {recipes: response.data.hits});
+        res.send(response.data);
+        // res.render("recipes", {recipes: response.data.hits});
     } catch (error) {
         res.send(new Error(error))
     }
