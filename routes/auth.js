3d97fbaa8b8822ddab7ba123e62a6d78e997e1f9
Diogo Marques <diogo@fontainhas.com>
2019-11-25 16:22:28 +0000

    routes for sign in

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,25 @@
 const router = require('express').Router();
+const passport = require('passport');
+
+
+// Sign In
+router.get('/sign-in', (req, res, next) => {
+  res.render('sign-in');
+});
+
+router.post('/sign-in/local', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/sign-in'
+}));
 
 
 router.get('/sign-up', (req, res, next) => {
-  res.render('auth');
+  res.render('sign-up');
 });
 
-router.post('/sign-up', (req, res, next) => {
-  res.render('auth');
-});
+router.post('/sign-up/local', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/sign-up'
+}));
 
 module.exports = router;
