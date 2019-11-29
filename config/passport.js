8a2bcd4d40f5bd761bf09ad0141e0eb2ddb00e0d
Diogo Marques <diogo@fontainhas.com>
2019-11-29 09:45:21 +0000

    email fixed

--- config/passport.js
+++ config/passport.js
@@ -81,7 +81,7 @@ passport.use('local', new LocalStrategy({ usernameField: 'email' }, async (email
         to: `${email}`,
         subject: 'UKA - Confirm Your Email',
         text: `Welcome to UKA. Go to 
-        http://localhost:3000/auth/confirm/${token} 
+        http://uka.recipes/auth/confirm/${token} 
         to confirm your email address`,
         html: htmlEmail
       });
